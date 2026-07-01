34ab49a041c1fd250b03977ff59a3689199140e6
Ghadah Alotaibi <75266002+ghadahalotaibi97@users.noreply.github.com>
2026-07-01 11:43:04 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,3 +25,105 @@ alert("Wrong username or password");
 }
 
 }
+function openForm(){
+
+document.getElementById("roadForm")
+.style.display="flex";
+
+}
+
+
+
+function closeForm(){
+
+document.getElementById("roadForm")
+.style.display="none";
+
+}
+
+
+
+
+function addRoad(){
+
+
+let no =
+document.getElementById("roadNo").value;
+
+
+let name =
+document.getElementById("roadName").value;
+
+
+let length =
+document.getElementById("length").value;
+
+
+
+let table =
+document.getElementById("roadsTable");
+
+
+
+let row =
+table.insertRow();
+
+
+
+row.innerHTML =
+`
+<td>${no}</td>
+<td>${name}</td>
+<td>New</td>
+<td>${length}</td>
+<td class="good">Good</td>
+`;
+
+
+
+closeForm();
+
+
+}
+
+
+
+
+function searchRoad(){
+
+
+let value =
+document.getElementById("search")
+.value.toLowerCase();
+
+
+
+let rows =
+document.querySelectorAll(
+"#roadsTable tr"
+);
+
+
+
+rows.forEach(row=>{
+
+
+if(row.innerText
+.toLowerCase()
+.includes(value)){
+
+row.style.display="";
+
+}
+
+else{
+
+row.style.display="none";
+
+}
+
+
+});
+
+
+}
